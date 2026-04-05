c95aea83f5b33a2af9c5a17076bbc5c53a5b3758
seekaddo <dennis_addo@aol.com>
2026-04-05 10:12:07 +0200

    build: update test target config for zig 0.15

--- build.zig
+++ build.zig
@@ -42,11 +42,12 @@ pub fn build(b: *std.Build) void {
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| run_cmd.addArgs(args);
 
-    const tests = b.addTest(.{
+    const test_module = b.createModule(.{
+        .root_source_file = b.path("src/test/all.zig"),
         .target = target,
         .optimize = optimize,
-        .root_source_file = b.path("src/test/all.zig"),
     });
+    const tests = b.addTest(.{ .root_module = test_module });
     const test_step = b.step("test", "Run tests");
     const run_tests = b.addRunArtifact(tests);
     if (b.args) |args| {
